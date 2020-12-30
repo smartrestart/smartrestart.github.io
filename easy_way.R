3d28e9536423bc1dc303e51f264a537d34034d6e
ruizcrp <ruiz.crp@gmail.com>
2020-12-30 22:31:16 +0100

    one file

--- easy_way.R
+++ easy_way.R
@@ -28,18 +28,20 @@ chbaseplus5<-read_excel("input/Corona_Virus_2020_291220.xlsx",sheet="CH 5 basepl
 
 nrow(chbaseplus5)
 names(chbaseplus5)[1]<-"Datum"
-names(chbaseplus5)[22]<-"Viral_Pot"
-names(chbaseplus5)[27]<-"Value_Infectious"
-names(chbaseplus5)[50]<-"Free_Viral"
+names(chbaseplus5)[22]<-"Viral_Pot_Plus"
+names(chbaseplus5)[27]<-"Value_Infectious_Plus"
+names(chbaseplus5)[50]<-"Free_Viral_Plus"
 
 nrow(chbaseplus5)
 #when date has no values it is additional data afterwards that should be deleted
 chbaseplus5<-chbaseplus5[!is.na(chbaseplus5[,1]),]
 #at the last 5 lines there are now till some NAs
-chbase5<-chbase5[!is.na(chbase5$Viral_Pot),]
+chbaseplus5<-chbaseplus5[!is.na(chbaseplus5$Viral_Pot_Plus),]
 
 #reduce cols
-chbase5<-chbase5[,c("Datum","Viral_Pot","Value_Infectious","Free_Viral")]
+chbaseplus5<-chbaseplus5[,c("Datum","Viral_Pot_Plus","Value_Infectious_Plus","Free_Viral_Plus")]
 #cbind()
-write.csv(chbase5,"output/data_scenario_chbase5.csv",row.names = FALSE)
-write.csv(chbaseplus5,"output/data_scenario_chbaseplus5.csv",row.names = FALSE)
+merged<-merge(chbase5,chbaseplus5, by="Datum")
+
+write.csv(merged,"output/data.csv",row.names = FALSE)
+
