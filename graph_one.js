a16f6b2dfbab79fe2535e7d4c8e28d99619d79cb
ruizcrp <ruiz.crp@gmail.com>
2020-12-30 23:06:00 +0100

    script link

--- graph_one.js
+++ graph_one.js
@@ -1,84 +1,6 @@
 
 
 
-
-
-<script>
-    function NMoveAvg(context, N) {
-      this._context = context;
-      this._points = {
-        x: [],
-        y: []
-      };
-      this._N = N;
-    }
-
-    NMoveAvg.prototype = {
-      areaStart: function() {
-        this._line = 0;
-      },
-      areaEnd: function() {
-        this._line = NaN;
-      },
-      lineStart: function() {
-        this._point = 0;
-      },
-      lineEnd: function() {
-        if (this._line || (this._line !== 0 && this._point === 1)) this._context.closePath();
-        this._line = 1 - this._line;
-      },
-      point: function(x, y) {
-        x = +x, y = +y;
-
-        this._points.x.push(x);
-        this._points.y.push(y);
-
-        if (this._points.x.length < this._N) return;
-
-        var aX = this._points.x.reduce(function(a, b) {
-            return a + b;
-          }, 0) / this._N,
-          aY = this._points.y.reduce(function(a, b) {
-            return a + b;
-          }, 0) / this._N;
-
-        this._points.x.shift();
-        this._points.y.shift();
-
-        switch (this._point) {
-          case 0:
-            this._point = 1;
-            this._line ? this._context.lineTo(aX, aY) : this._context.moveTo(aX, aY);
-            break;
-          case 1:
-            this._point = 2; // proceed
-          default:
-            this._context.lineTo(aX, aY);
-            break;
-        }
-      }
-    };
-
-    var curveNMoveAge = (function custom(N) {
-
-      function nMoveAge(context) {
-        return new NMoveAvg(context, N);
-      }
-
-      nMoveAge.N = function(N) {
-        return custom(+N);
-      };
-
-      return nMoveAge;
-    })(0);
-  </script>
-
-
-<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
-
-
-<script>
-
 // set the dimensions and margins of the graph
 var margin = {top: 50, right: 150, bottom: 110, left: 50},
     width = 1200 - margin.left - margin.right,
@@ -487,6 +409,3 @@ d3.csv("https://raw.githubusercontent.com/smartrestart/smartrestart.github.io/ma
     .style("font-size", 15)
 
 })
-
-
-</script>
