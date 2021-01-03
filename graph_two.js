abbc197981524ccb701932e11ea02d0d80c937ef
ruizcrp <ruiz.crp@gmail.com>
2021-01-03 18:47:06 +0100

    include new sources and descriptive text

--- graph_two.js
+++ graph_two.js
@@ -14,11 +14,11 @@ var svg = d3.select("#my_dataviz2")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
-d3.csv("https://raw.githubusercontent.com/smartrestart/smartrestart.github.io/master/output/lastupdate.csv", function(data) {
+d3.csv("https://smartrestartch.s3.eu-central-1.amazonaws.com/lastupdate.csv", function(data) {
   lastupdatestring=data[0]['lastupdate'];
 });
 //Read the data
-d3.csv("https://raw.githubusercontent.com/smartrestart/smartrestart.github.io/master/output/data.csv", function(data) {
+d3.csv("https://smartrestartch.s3.eu-central-1.amazonaws.com/data.csv", function(data) {
     // List of groups (here I have one group per column)
     var allGroup = ["Viralität_pro_X"];
     var secondGroup = ["Viralität_in_Proz"];
